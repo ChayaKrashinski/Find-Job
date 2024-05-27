83508441219150ee462fccf081846fad3218cf40
ChayaKrashinski <152270039+ChayaKrashinski@users.noreply.github.com>
2024-05-27 18:58:04 +0300

    Delete src directory

--- src/app/models/user.ts
+++
@@ -1,8 +0,0 @@
-import { Profession } from "./profession"
-
-export interface User{
-    id:number
-    name:string
-    password:string
-    profession:Profession
-}
--- src/app/pipes/profession-name.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { ProfessionNamePipe } from './profession-name.pipe';
-
-describe('ProfessionNamePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ProfessionNamePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
